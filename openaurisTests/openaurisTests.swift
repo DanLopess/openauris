3496b4efb2e05f0b6522bd62c12070cd2eed9521
Daniel Lopes <daniellopesguerra@gmail.com>
2026-02-23 20:34:24 +0100

    Initial Commit

---
+++ openaurisTests/openaurisTests.swift
@@ -0,0 +1,17 @@
+//
+//  openaurisTests.swift
+//  openaurisTests
+//
+//  Created by Daniel Lopes on 23.02.2026.
+//
+
+import Testing
+@testable import openauris
+
+struct openaurisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
